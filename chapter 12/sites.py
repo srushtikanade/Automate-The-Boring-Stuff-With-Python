e957d53a9c35d896dd72538debc7d47b06664079
srushtikanade <63073854+srushtikanade@users.noreply.github.com>
2020-06-27 23:06:26 +0530

    Create sites.py

---
+++ chapter 12/sites.py
@@ -0,0 +1,16 @@
+import webbrowser,sys
+
+if len(sys.argv)>1:
+    if sys.argv[1]=='atbs':
+        webbrowser.open('https://automatetheboringstuff.com/')
+    if sys.argv[1]=='instagram':
+        webbrowser.open('https://www.instagram.com/srushtiikanade/')
+    if sys.argv[1]=='github':
+        webbrowser.open('https://github.com/srushtikanade')
+    if sys.argv[1]=='gmail':
+        webbrowser.open('https://mail.google.com/mail/u/0/#inbox')
+    if sys.argv[1]=='reddit':
+        webbrowser.open('https://www.reddit.com/user/primadonnasrush')
+    if sys.argv[1]=='coursera':
+        webbrowser.open('https://www.coursera.org/')
+        
