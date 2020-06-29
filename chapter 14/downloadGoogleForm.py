ee65ba4102d600b31bfd6accad3cd3f9751ac810
srushtikanade <63073854+srushtikanade@users.noreply.github.com>
2020-06-29 22:23:32 +0530

    Update downloadGoogleForm.py

--- chapter 14/downloadGoogleForm.py
+++ chapter 14/downloadGoogleForm.py
@@ -3,6 +3,6 @@ ss = ezsheets.Spreadsheet('1s959a0f3zlr63pjrocryiX7mnxE7YU8jeHOxh4lqEtk')
 ss.title
 ss.sheetTitles
 rows = sheet.getRows()
-for i in range(rows):
+for i in range(rows): # or enumerate
     rows[i]
     
