72ed3a2e698211126396f762c05a9c77bc49fe2e
srushtikanade <63073854+srushtikanade@users.noreply.github.com>
2020-06-29 22:22:04 +0530

    Create downloadGoogleForm.py

---
+++ chapter 14/downloadGoogleForm.py
@@ -0,0 +1,8 @@
+import ezsheets
+ss = ezsheets.Spreadsheet('1s959a0f3zlr63pjrocryiX7mnxE7YU8jeHOxh4lqEtk')
+ss.title
+ss.sheetTitles
+rows = sheet.getRows()
+for i in range(rows):
+    rows[i]
+    
