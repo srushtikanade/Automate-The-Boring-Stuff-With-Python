eba72132cc3807289d848294ce3c440ad3689346
srushtikanade <63073854+srushtikanade@users.noreply.github.com>
2020-06-30 19:24:50 +0530

    Update excelToCsv.py

--- chapter 16/excelToCsv.py
+++ chapter 16/excelToCsv.py
@@ -15,7 +15,7 @@ for file in os.listdir('.'):
 
             for rowNum in range(1,sheet.max_row+1):
                 rowData=[]
-                for columnNum in range(1,sheet.max+1):
+                for columnNum in range(1,sheet.max_column+1):
                     rowData.append(sheet.cell(row=rowNum,column=columnNum).value)
                 for row in rowData:
                     csvWriter.writerow(row)
